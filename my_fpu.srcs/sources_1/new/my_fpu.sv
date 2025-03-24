dbf0e3c2ab19d0f21d3385c1a8f1fba9fe1ac88d
Karlo Godfrey Escalona Gregorio <kgregorio9@yahoo.com>
2025-03-23 21:46:31 -0700

    added gitignore, initial FPU file

---
+++ my_fpu.srcs/sources_1/new/my_fpu.sv
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/23/2025 09:18:27 PM
+// Design Name: 
+// Module Name: my_fpu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_fpu(
+    output [39:0] out,
+    input [31:0] in1,
+    input [31:0] in2,
+    input sel
+    );
+    assign out = sel ? in1 + in2 : in1 + 1;
+endmodule
